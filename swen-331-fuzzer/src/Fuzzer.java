3e63c80e14abff86832067d5c9398caf13276358
unknown <zpc4747@AUBREY.se.rit.edu>
2014-10-13 21:42:22 -0400

    Added stubs to test method

--- swen-331-fuzzer/src/Fuzzer.java
+++ swen-331-fuzzer/src/Fuzzer.java
@@ -165,17 +165,22 @@ public class Fuzzer {
 	public void test() throws FailingHttpStatusCodeException, MalformedURLException, IOException
 	{
 		discover();
-		ArrayList<String> allPages = new ArrayList<String>();
+		List<swen.fuzzer.report.Page> allPages = this.report.getPages();
 		ArrayList<String> allFormPages = new ArrayList<String>();
-		for(String url : allPages)
+		for(swen.fuzzer.report.Page url : allPages)
 		{
-			final HtmlPage pg = webClient.getPage(url);
+			final HtmlPage pg = webClient.getPage(url.getURL().toString());
 			DomElement  form = pg.getElementById("form");
 			if(form != null){
 				allFormPages.add(pg.getUrl().toString());
 			}
 		}
 		
+		
+		//run vectors on allFormPages
+		
+		
+		//Analyze all pages 
 	}
 	
 	/*
